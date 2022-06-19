6fbb7d5a64afdd6de45b73f451f92333793a57fd
petervanderhook <petervanderhook@gmail.com>
2022-06-19 01:25:15 -0700

    Modified Menu

---
+++ levels/win_level_2.gd
@@ -0,0 +1,23 @@
+extends Area
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var scene_manager = get_tree().get_root().get_node("Robbie")
+onready var player = get_parent().get_parent().get_node_or_null("Player")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+
+func _on_Area_body_entered(body):
+	if body == player:
+		scene_manager.load_level('level3')
