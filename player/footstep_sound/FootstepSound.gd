aaedeb6ab25ab9dc7a21d23e0d31a0093eb3be4f
Spencer A. Lockhart <s-p-n@users.noreply.github.com>
2022-06-18 19:50:33 -0600

    walking sound setup

--- player/footstep_sound/FootstepSound.gd
+++ player/footstep_sound/FootstepSound.gd
@@ -1,18 +1,10 @@
 extends Spatial
 
-export (Resource) var footstep_sound1
-export (Resource) var footstep_sound2
-export (Resource) var footstep_sound3
-export (Resource) var footstep_sound4
-export (Resource) var footstep_sound5
-export (Resource) var footstep_sound6
-
-
-onready var footstep_sounds = [footstep_sound1, footstep_sound2, footstep_sound3, footstep_sound4, footstep_sound5, footstep_sound6, footstep_sound7, footstep_sound8, footstep_sound9, footstep_sound10]
+export(Array, AudioStream) var footstep_sounds = []
 
 onready var player = get_tree().get_root().find_node("Player", true ,false)
 
-func _process(delta):
+func _process(_delta):
 	if $Timer.is_stopped():
 		if player.is_on_floor() and player.player_speed >= 2:
 			var animation_speed = 1.0 / (player.player_speed / 2)
@@ -30,6 +22,8 @@ func _process(delta):
 
 func play_sound(volume): # To avoid the sound from clipping, we generate a new audio node each time then we delete it
 	var audio_node = AudioStreamPlayer.new()
+	if len(footstep_sounds) == 0:
+		return
 	var pick_sound = randi() % footstep_sounds.size() # Pick a random sound
 	audio_node.stream = footstep_sounds[pick_sound]
 	audio_node.volume_db = volume
