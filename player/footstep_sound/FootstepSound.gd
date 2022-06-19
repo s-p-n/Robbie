601d33d03d1e99e1db1a7710487a94ebb0d354a6
Spencer A. Lockhart <s-p-n@users.noreply.github.com>
2022-06-18 20:17:46 -0600

    fix footstep switch level bug

--- player/footstep_sound/FootstepSound.gd
+++ player/footstep_sound/FootstepSound.gd
@@ -1,11 +1,20 @@
 extends Spatial
 
 export(Array, AudioStream) var footstep_sounds = []
+onready var root = get_tree().get_root()
+onready var active_level = root.get_node("Robbie/ActiveLevel").get_child(0)
+var player
 
-onready var player = get_tree().get_root().find_node("Player", true ,false)
-
+func _ready():
+	if active_level:
+		set_player()
+	
 func _process(_delta):
 	if $Timer.is_stopped():
+		set_player()
+		if !player:
+			print("player is falsey")
+			return
 		if player.is_on_floor() and player.player_speed >= 2:
 			var animation_speed = 1.0 / (player.player_speed / 2)
 			if round(player.player_speed) > 3:
@@ -19,6 +28,13 @@ func _process(_delta):
 	
 	if player.is_on_floor() and player.snapped == false:
 		play_sound(-10)
+
+func set_player():
+	var active_level = root.get_node("Robbie/ActiveLevel").get_child(0)
+	if active_level:
+		player = active_level.get_node_or_null("Player")
+	else:
+		player = null
 
 func play_sound(volume): # To avoid the sound from clipping, we generate a new audio node each time then we delete it
 	var audio_node = AudioStreamPlayer.new()
