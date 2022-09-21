e95240385ddc5e8dd64a96512f4fe900fe6c96b3
Spencer A. Lockhart <s-p-n@users.noreply.github.com>
2022-09-21 06:06:13 -0600

    work a little on shop

--- modules/Shop/Shop.gd
+++ modules/Shop/Shop.gd
@@ -1,5 +1,7 @@
 extends Control
 
+onready var itemList:ItemList = $Panel/ItemList
+
 var active = false
 
 func _ready():
@@ -14,8 +16,14 @@ func deactivate():
 	active = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	visible = false
-	
-
 
 func _on_Close_pressed():
 	deactivate()
+
+func _on_Buy_pressed():
+	var items = itemList.get_selected_items()
+	if len(items) == 0:
+		return
+	var item = itemList.get_item_text(items[0])
+	print("User wants to buy: ", item)
+	
