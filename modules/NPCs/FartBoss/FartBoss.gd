53f316ae437be6ae839c7ca3acec99db0c6ed657
Spencer A. Lockhart <s-p-n@users.noreply.github.com>
2022-10-05 23:03:10 -0600

    make boss have health, and also able to hit lol

--- modules/NPCs/FartBoss/FartBoss.gd
+++ modules/NPCs/FartBoss/FartBoss.gd
@@ -1,10 +1,63 @@
 extends KinematicBody
 
 export var cloudScene:PackedScene
+export var health:int = 10
+export var stamina:float = 100
+
+onready var healthBarGreen = $HealthBarGreen
+onready var healthBarRed = $HealthBarRed
+
+var start_health = 10
+var spawn_pos:Vector3
+
+func _ready():
+	spawn_pos = global_transform.origin
+	start_health = health
 
 func _on_Butt_body_entered(body):
 	if body.name == "Player":
-		var cloud = cloudScene.instance()
-		cloud.global_transform.origin = global_transform.origin - Vector3(0,1,0)
-		get_parent().add_child(cloud)
-		print("createdd fart cloud")
+		if is_instance_valid(self):
+			var cloud = cloudScene.instance()
+			cloud.global_transform.origin = global_transform.origin - Vector3(0,1,0)
+			get_parent().add_child(cloud)
+			print("createdd fart cloud")
+
+func respawn():
+	health = start_health
+	global_transform.origin = spawn_pos
+	update_health_visuals()
+
+func adjust_stamina(_n):
+	#stamina += n
+	pass
+
+func get_stamina():
+	return stamina
+
+func die():
+	visible = false
+	$Brain.die(self)
+
+func handle_death():
+	print("Fart Boss dead")
+	queue_free()
+
+func interact():
+	health -= 1
+	
+	if health < 0:
+		die()
+	
+	update_health_visuals()
+
+func update_health_visuals():
+	for i in range(10):
+		var green_mesh:MeshInstance = healthBarGreen.get_child(9 - i)
+		var red_mesh:MeshInstance = healthBarRed.get_child(9 - i)
+		
+		if i <= health:
+			green_mesh.visible = true
+		else:
+			green_mesh.visible = false
+		
+		red_mesh.visible = !green_mesh.visible
