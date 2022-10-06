8e8b58e16e7ae7cb294e9a580237a21f6be4ccfe
Spencer A. Lockhart <s-p-n@users.noreply.github.com>
2022-10-06 14:16:27 -0600

    work on scrap and litter first level with scrap

--- modules/Interactive/PowerUp/PowerUp.gd
+++ modules/Interactive/PowerUp/PowerUp.gd
@@ -3,14 +3,32 @@ extends Spatial
 export(String, "Select Power", "Laser Gun", "Extra Life", "Wings") var power
 
 onready var shape:RigidBody = $Shape
+onready var collectSound:AudioStreamPlayer3D = $CollectSound
+
+var direction = true
 
 func _physics_process(delta):
 	var r = delta * 50
-	shape.rotation_degrees += Vector3(r, r, r)
 	
+	if r > 360:
+		direction = false
+	elif r < -359:
+		direction = true
+	
+	if direction:
+		shape.rotation_degrees += Vector3(r, r, r)
+	else:
+		shape.rotation_degrees -= Vector3(r, r, r)
 
 func _on_Shape_body_entered(body):
 	print("collision with power-up: ", body)
 	if is_instance_valid(body) and body.name == "Player":
 		body.give_power(power)
-		queue_free()
+		
+		if !collectSound.playing:
+			collectSound.play(0)
+			shape.visible = false
+			collectSound.connect("finished", self, "_handle_removal")
+
+func _handle_removal():
+	queue_free()
---
+++ modules/Interactive/ScrapParts/part_rotate.gd
@@ -0,0 +1,19 @@
+extends MeshInstance
+
+var direction = true
+
+func _ready():
+	rotation_degrees = Vector3(rand_range(-360.0, 360.0), rand_range(-360.0, 360.0), rand_range(-360.0, 360.0))
+
+func _physics_process(delta):
+	var r = delta * 50
+	
+	if r > 360:
+		direction = false
+	elif r < -359:
+		direction = true
+	
+	if direction:
+		rotation_degrees += Vector3(r, r, r)
+	else:
+		rotation_degrees -= Vector3(r, r, r)
