cc8b4f03f2009c954166a404c939f18b4cfe26cc
Spencer A. Lockhart <s-p-n@users.noreply.github.com>
2022-10-06 11:42:32 -0600

    remove friend from level 3

--- modules/Levels/Boss1.gd
+++ modules/Levels/Boss1.gd
@@ -7,7 +7,8 @@ func _ready():
 	old_music = audio_node.stream
 	audio_node.stream = music
 	audio_node.play(0)
-	$FartBoss.connect("boss_dead", self, "_handle_boss_death")
+	if !$FartBoss.connect("boss_dead", self, "_handle_boss_death"):
+		print("Warning! Boss level cannot connect death signal from Boss")
 
 func _exit_tree():
 	get_parent().get_parent().get_node("AudioStreamPlayer").stream = old_music
